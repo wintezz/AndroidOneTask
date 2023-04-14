2f8482b4da4ca6e7a42d6bc06fae5203840902b7
wintezz <saberbringers@gmail.com>
2023-04-14 17:36:55 +0300

    fixed comments

--- app/src/main/java/com/example/androidonetask/data/repository/Repository.kt
+++ app/src/main/java/com/example/androidonetask/data/repository/Repository.kt
@@ -2,20 +2,25 @@ package com.example.androidonetask.data.repository
 
 import com.example.androidonetask.data.ApiService
 import com.example.androidonetask.data.ApiService.Companion.BASE_URL
+import com.example.androidonetask.data.AppState
 import com.example.androidonetask.data.RetrofitClient
 import com.example.androidonetask.data.model.TrackListResponse
+import retrofit2.HttpException
+import java.io.IOException
 
 object Repository {
 
     private val retrofitService: ApiService =
-        RetrofitClient.getClient(BASE_URL)
-            .create(ApiService::class.java)
+        RetrofitClient.getClient(BASE_URL).create(ApiService::class.java)
 
-    fun getTracks(): TrackListResponse? {
+    fun getTracks(): AppState<TrackListResponse>? {
         return try {
-            retrofitService.getTrackList().execute().body()
-        } catch (e: Throwable) {
-            TrackListResponse(emptyList())
+            val track = retrofitService.getTrackList().execute().body()
+            AppState.Success(response = track)
+        } catch (e: HttpException) {
+            AppState.Error(error = e)
+        } catch (e: IOException) {
+            AppState.Error(error = e)
         }
     }
 }
